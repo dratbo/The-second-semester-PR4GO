8465d0b9430b31258c4e72779fbbe947ee989a9a
dratbo <dratbo03@mail.ru>
2026-04-07 00:10:20 +0300

    Add files via upload

---
+++ pz4-monitoring/internal/metrics/metrics.go
@@ -0,0 +1,40 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	HttpRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "app_http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "path"},
+	)
+
+	HttpErrorsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "app_http_errors_total",
+			Help: "Total number of HTTP error responses",
+		},
+		[]string{"method", "path", "status_code"},
+	)
+
+	HttpRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "app_http_request_duration_seconds",
+			Help:    "HTTP request duration in seconds",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+	StudentRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "app_student_requests_total",
+			Help: "Total number of requests to /students/{id} by student ID",
+		},
+		[]string{"student_id"},
+	)
+)
